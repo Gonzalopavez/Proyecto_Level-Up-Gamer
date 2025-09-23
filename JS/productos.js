f14203a3febf812db1e5d89bcf8e8b0141b00940
gonzalo <go.pavez@duocuc.cl>
2025-09-22 23:47:41 -0300

    reorganizacion de codigos

--- JS/productos.js
+++ JS/productos.js
@@ -12,30 +12,30 @@ const listaProductos = [
     id: "prod-02",
     nombre: "Procesador AMD Ryzen 9",
     precio: 549990,
-    imagen: "img/productos/ryzen9.png"
+    imagen: "img/amd.webp"
   },
   {
     id: "prod-03",
     nombre: "Monitor Gamer Curvo 32\"",
     precio: 349990,
-    imagen: "img/productos/monitor-curvo.png"
+    imagen: "img/curvo.jpg"
   },
   {
     id: "prod-04",
     nombre: "Teclado Mecánico RGB",
     precio: 89990,
-    imagen: "img/productos/teclado-rgb.png"
+    imagen: "img/teclado.jpg"
   },
   {
     id: "prod-05",
     nombre: "Mouse Gamer Inalámbrico",
     precio: 64990,
-    imagen: "img/productos/mouse-gamer.png"
+    imagen: "img/mouse1.jpg"
   },
   {
     id: "prod-06",
     nombre: "Audífonos Gamer 7.1",
     precio: 79990,
-    imagen: "img/productos/audifonos-gamer.png"
+    imagen: "img/audifonos.jpg"
   }
 ];
